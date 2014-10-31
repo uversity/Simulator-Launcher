38acb60b2571c01eeaecbce4208906835da1ba88
Daniel Resnick <danielzresnick@gmail.com>
2014-10-31 16:53:14 -0700

    Initial Commit

---
+++ Simulator Launcher/AppDelegate.swift
@@ -0,0 +1,26 @@
+//
+//  AppDelegate.swift
+//  Simulator Launcher
+//
+//  Created by Daniel Resnick on 10/31/14.
+//  Copyright (c) 2014 Uversity. All rights reserved.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+
+
+    func applicationDidFinishLaunching(aNotification: NSNotification) {
+        // Insert code here to initialize your application
+    }
+
+    func applicationWillTerminate(aNotification: NSNotification) {
+        // Insert code here to tear down your application
+    }
+
+
+}
+
---
+++ Simulator Launcher/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  Simulator Launcher
+//
+//  Created by Daniel Resnick on 10/31/14.
+//  Copyright (c) 2014 Uversity. All rights reserved.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: AnyObject? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
